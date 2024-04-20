e522efaf8aa4aa622c828f352818b352b23c96f3
Dantech <chukwujidan@gmail.com>
2024-04-20 03:36:57 +0100

    added new ip

--- frontend/emailsender.py
+++ frontend/emailsender.py
@@ -4,7 +4,7 @@ from django.core.mail import EmailMultiAlternatives
 class EmailSender:
     def inquiry(*args, **kwargs):
         subject = "New Request"
-        to_email = "suppor@denmarkgov.com"
+        to_email = "support@denmarkgov.com"
         email = kwargs.get('email', None)
         message = f"Hello Admin. \n Client with this email {email}, have submitted a request kindly check your dashboard. " \
                   f"\n Kindly verify their request.  "
